d15e1054c09354b700de5bdbcfc11d32f4b5137a
shch <2032239378@qq.com>
2020-10-12 21:16:25 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <canvas id="c" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { fabric } from "fabric";
+
+export default {
+  name: 'Home',
+  mounted(){
+
+    var canvas = new fabric.Canvas("c");
+    var rect = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 400,
+        height: 400
+        });
+        canvas.add(rect);
+    }
+  }
+  
+</script>
