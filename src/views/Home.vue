24448643326e50c7ea3d591faf5ae555bef4b14a
shch <2032239378@qq.com>
2020-10-21 22:34:07 +0800

    add Line

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,8 @@
          <label >画笔线条粗细:</label>
         <input type="range" name="vol" min="1" max="50" v-model="width">
       </div>
+      <button @click="freeDraw()" class="freeDrawBtn">自由绘制</button>
+      <button @click="lineDraw()" class="lineDrawBtn">直线</button>
       <button @click="clear()" class="clearBtn">清除</button>
     </div>
     <canvas id="c" width="1440" height="600"></canvas>
@@ -27,15 +29,22 @@ export default {
   data () {
     return {
       colors:'#000000',
-      width:10,
+      width:1,
+      strokeWidth:1,
       showColorPicker:false,
-      canvas:null
+      canvas:null,
+      drawingObject:null,
+      mouseFrom: {},
+      mouseTo: {},
+      currentType:'free',
+      idDrawing:false,
     }
   },
   watch: {
     width: function (val) {
       console.log(val);
       this.canvas.freeDrawingBrush.width = parseInt(this.width, 10);
+      this.strokeWidth = parseInt(this.width, 10);
     },
   },
   methods:{
@@ -51,7 +60,7 @@ export default {
     changeBrushColor(){
       this.showColorPicker = !this.showColorPicker;
     },
-    freeDraw(){
+    initfreeDraw(){
       if(this.canvas == null){
         this.canvas = new fabric.Canvas("c");
         this.canvas.backgroundColor = '#efefef';
@@ -61,14 +70,70 @@ export default {
       this.canvas.freeDrawingBrush.width = this.width;
       this.canvas.renderAll();
     },
+    freeDraw(){
+      this.canvas.isDrawingMode = true;
+      this.currentType = 'free';
+      this.resetLine();
+    },
+    lineDraw(){
+      this.currentType = 'line';
+      this.ininLine();
+      this.resetLine();
+    },
+    ininLine(){
+      this.canvas.isDrawingMode = false;
+      this.canvas.selection = false;
+      let canvasObject = new fabric.Line([this.mouseFrom.x, this.mouseFrom.y, this.mouseTo.x, this.mouseTo.y], {
+        fill: this.colors,
+        stroke: this.colors,
+        strokeWidth: this.strokeWidth
+      });
+      canvasObject.selectable = false;
+      if (this.drawingObject) {
+        this.canvas.remove(this.drawingObject);
+      }
+      this.canvas.add(canvasObject);
+      this.drawingObject = canvasObject;
+    },
+    initEvent(){
+      this.canvas.on('mouse:down', (options) => {
+        if(this.currentType == 'line'){
+          this.mouseFrom.x = options.e.clientX;
+          this.mouseFrom.y = options.e.clientY - 50;
+          this.idDrawing = true;
+        }
+      });
+      this.canvas.on('mouse:move', (options) => {
+        if(this.idDrawing && this.currentType == 'line'){
+          this.mouseTo.x = options.e.clientX;
+          this.mouseTo.y = options.e.clientY - 50;
+          this.ininLine();
+        }
+      });
+      this.canvas.on("mouse:up", (options) => {
+        if(this.currentType == 'line'){
+          this.mouseTo.x = options.e.clientX;
+          this.mouseTo.y = options.e.clientY - 50;
+          this.drawingObject = null;
+          this.idDrawing = false;
+        }
+      }); 
+       
+    },
+    resetLine(){
+      this.mouseFrom = {};
+      this.mouseTo = {};
+    },
     clear(){
       this.canvas.clear();
       this.canvas.backgroundColor = '#efefef';
+      this.resetLine();
     }
   },
   mounted(){
     this.initBrushColor();
-    this.freeDraw();
+    this.initfreeDraw();
+    this.initEvent();
   }
 
 }
@@ -98,7 +163,8 @@ export default {
   .brushWidth{
     margin-left: 10px;
   }
-  .clearBtn{
+  
+  .freeDrawBtn,.lineDrawBtn,.clearBtn{
     margin-left: 10px;
     &:hover{
       cursor: pointer;
